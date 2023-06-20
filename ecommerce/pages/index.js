6a89d3832bf5dc925502c137e5141a7b23053f8b
MountainBikerYYC <ywang9696@gmail.com>
2023-06-19 19:48:40 -0600

    Convert fetch to server side

--- ecommerce/pages/index.js
+++ ecommerce/pages/index.js
@@ -1,36 +1,60 @@
 import { useEffect, useState } from "react";
 import Product from "@/components/Product";
+import { initMongoose } from "@/lib/mongoose";
+import { findAllProducts } from "./api/products";
 
-export default function Home() {
-  const [productsInfo, setProductsInfo] = useState([]);
-  useEffect(() => {
-    fetch("api/products")
-      .then((response) => response.json())
-      .then((json) => setProductsInfo(json));
-  }, []);
+export default function Home({ products }) {
+  //   const [productsInfo, setProductsInfo] = useState([]);
+  const [phrase, setPhrase] = useState("");
 
-  const categoriesNames = [...new Set(productsInfo.map((p) => p.category))];
-  console.log(productsInfo);
+  const categoriesNames = [...new Set(products.map((p) => p.category))];
+  //   console.log(productsInfo);
+
+  if (phrase) {
+    products = products.filter((p) => p.name.toLowerCase().includes(phrase));
+  } else {
+    products = products;
+  }
 
   return (
     <div className="p-5">
+      <input
+        value={phrase}
+        type="text"
+        placeholder="Search for products..."
+        className="bg-gray-100 w-fyll py-2 px-4 rounded-xl"
+        onChange={(e) => setPhrase(e.target.value)}
+      ></input>
       <div>
         {categoriesNames.map((categoryName) => (
           <div key={categoryName}>
-            <h2 className="text-2xl capitalize">{categoryName}</h2>
-            <div className="flex -mx-5 overflow-x-scroll snap-x scrollbar-hide">
-              {productsInfo
-                .filter((p) => p.category === categoryName)
-                .map((product) => (
-                  <div className="px-5" key={product._id}>
-                    <Product product={product} />
-                  </div>
-                ))}
-            </div>
+            {products.find((p) => p.category === categoryName) && (
+              <div>
+                <h2 className="text-2xl py-5 capitalize">{categoryName}</h2>
+                <div className="flex -mx-5 overflow-x-scroll snap-x scrollbar-hide">
+                  {products
+                    .filter((p) => p.category === categoryName)
+                    .map((product) => (
+                      <div className="px-5 snap-start" key={product._id}>
+                        <Product product={product} />
+                      </div>
+                    ))}
+                </div>
+              </div>
+            )}
           </div>
         ))}
-        <div className="py-4"></div>
       </div>
     </div>
   );
+}
+
+export async function getServerSideProps() {
+  await initMongoose();
+  const products = await findAllProducts();
+  return {
+    props: {
+      products: JSON.parse(JSON.stringify(products)),
+    },
+  };
 }
